4f9e4f7d37a107f40a3799ea0f24c94cf0d4fb00
vader <pradyuishere@gmail.com>
2019-02-17 07:10:22 -0800

    pooling layer tested

--- layer.py
+++ layer.py
@@ -21,33 +21,50 @@ def conv_layer(input_img, num_kernels, nonlinear_func, kernels, stride = (1, 1),
 
     return img_out
 ################################################################################
+##Remember to give it and array of images, not a 3 dim input, array of 2d inputs
 def pool_layer(input_img, pool_func, pool_window=(1,1), stride = (1,1)):
-    img_out = np.zeros((int((dimy-pool_window[0])/stride[0])+1, (int((dimx-pool_window[1])/stride[1])+1), input_img.shape[2]))
-    for iter in range(input_img.shape[2]):
-        img_out[:, :, iter] = pooling(input_img[:, :, iter], pool_func, pool_window, stride)
+    dimy = input_img[0].shape[0]
+    dimx = input_img[0].shape[1]
+    img_out = np.zeros((int((dimy-pool_window[0])/stride[0])+1, (int((dimx-pool_window[1])/stride[1])+1), input_img.shape[0]))
+    for iter in range(input_img.shape[0]):
+        print(input_img[1].shape)
+        img_out[:, :, iter] = pooling(input_img[iter], pool_func, pool_window, stride)
     return img_out
 ################################################################################
+##Testing the conv layer
 
-
+# img = cv2.imread('image.png')
+# ker1 = np.zeros((10, 10, 3))
+# ker12 = np.ones((10, 10, 3))/30000
+# ker1[5, 5, :] = 1
+# # print(ker1)
+# ker2 = []
+# ker2.append(ker1)
+# ker2.append(ker12)
+# img_out1 = conv_layer(img, 2, nonlinear_func, np.array(ker2),  stride = (5, 5), pad = 'valid')
+# # img_out1 = pooling(img_out1, pool_func, pool_window=(2,2), stride = (2,2))
+# # img_out1 = conv2d(img, ker1*1000, nonlinear_func, stride = (5, 5), pad = 'valid')
+# # plt.imshow(img_out1, cmap = 'gray')
+# #img_out1 = padding(img, img.shape[1]+10, img.shape[0]+1000)
+# # print(img_out1.shape[2])
+#
+# fig = plt.figure(figsize=(img_out1.shape[0], img_out1.shape[1]))  # width, height in inches
+#
+# for i in range(img_out1.shape[2]):
+#     sub = fig.add_subplot(img_out1.shape[2], 1, i + 1)
+#     sub.imshow(img_out1[:,:, i], cmap = 'gray', norm=None)
+#
+# plt.show()
+################################################################################
+##Testing the pooling layer
+####Testing the pooling_layer
 img = cv2.imread('image.png')
-ker1 = np.zeros((10, 10, 3))
-ker12 = np.ones((10, 10, 3))/30000
-ker1[5, 5, :] = 1
-# print(ker1)
-ker2 = []
-ker2.append(ker1)
-ker2.append(ker12)
-img_out1 = conv_layer(img, 2, nonlinear_func, np.array(ker2),  stride = (5, 5), pad = 'valid')
-# img_out1 = pooling(img_out1, pool_func, pool_window=(2,2), stride = (2,2))
-# img_out1 = conv2d(img, ker1*1000, nonlinear_func, stride = (5, 5), pad = 'valid')
-# plt.imshow(img_out1, cmap = 'gray')
+imgs = []
+imgs.append(img[:, :, 0])
+imgs.append(np.random.randint(25, size = (img.shape[0], img.shape[1])))
+# print(imgs[0].shape)
+# img_out1 = conv2d(img, ker1, nonlinear_func, stride = (5, 5), pad = 'valid')
+img_out1 = pool_layer(np.array(imgs), pool_func, pool_window=(2,2), stride = (2,2))
 #img_out1 = padding(img, img.shape[1]+10, img.shape[0]+1000)
-# print(img_out1.shape[2])
-
-fig = plt.figure(figsize=(img_out1.shape[0], img_out1.shape[1]))  # width, height in inches
-
-for i in range(img_out1.shape[2]):
-    sub = fig.add_subplot(img_out1.shape[2], 1, i + 1)
-    sub.imshow(img_out1[:,:, i], cmap = 'gray', norm=None)
-
+plt.imshow(img_out1[:, :, 1].astype(int), cmap = 'gray')
 plt.show()
