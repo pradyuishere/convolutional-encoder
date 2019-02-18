74c69e3b4af96f29d6c8a1b842fe6e063dd45c01
vader <pradyuishere@gmail.com>
2019-02-18 01:02:28 -0800

    q6 unravel done

--- unravel.py
+++ unravel.py
@@ -2,3 +2,11 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+
+
+def unravel(input_img, weight):
+    input_row = np.matrix(input_img.flatten())
+    input_col = np.transpose(input_row)
+#     print(weight.shape)
+    prod = np.matmul(np.transpose(weight), input_col)
+    return prod
