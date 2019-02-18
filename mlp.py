c7e4774a17c24d328a9d53cd526aabf5cea727cf
vader <pradyuishere@gmail.com>
2019-02-18 01:33:55 -0800

    jupyter notebook gives two outputs, softmax and without

--- mlp.py
+++ mlp.py
@@ -6,5 +6,6 @@ import matplotlib.image as mpimg
 def mlp(col_mat_in, num_layers, layer_sizes, weights, biases, activation_funcs):
     prod_out = col_mat_in
     for iter in range(num_layers):
-        prod_out = activation_funcs[iter](np.matmul(np.transpose(weight), prod_out)+biases)
+#         this_wt = weights[iter]
+        prod_out = activation_funcs[iter](np.matmul(np.transpose(weights[iter]), prod_out)+biases[iter])
     return prod_out
